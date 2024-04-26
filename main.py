6ce376cce2c1c48eb408ba2057869dbabf145f07
Paweł Bernaciak <pawelbernaciak@zohomail.eu>
2024-04-26 17:51:18 +0200

    Teraz działa

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from network import SOM
 import tkinter as tk
-from random import random
-from typing import List   
+from random import random  
+
+NEURON_SIZE: int = 5
 
 def update():
-    input: List[float] = [random() for _ in range(2)]
-    som.learn(input)
-    som.draw(canvas, neuron_size)
-    root.after(10, update)
+    x, y = random(), random()
+
+    if (abs((x - 0.5) / 0.5) + abs((y - 0.5) / 0.5)) <= 1:
+        som.learn([x, y])
+
+    som.draw(canvas, NEURON_SIZE)
+
+    root.after(1, update)
 
 if __name__ == "__main__":
     root = tk.Tk()
     canvas = tk.Canvas(root, width=1000, height=1000)
     canvas.pack()
 
-    som = SOM(10, 2, 0.1, 0.9999, 0.999)
+    som = SOM(15, 15, 2, 0.15, 0.99999, 0.999)
 
-    neuron_size = 5
-    neuron_padding = 1000 / len(som.neurons)
-        
-    root.after(10, update)
+    root.after(1, update)
 
     root.mainloop()
--- network.py
+++ network.py
@@ -3,37 +3,40 @@ from typing import List
 from math import sqrt, exp
 from tkinter import Canvas
 
+
 class Neuron:
     def __init__(self, num_dimensions: int) -> None:
-        #self.weights: List[float] = [random.random() / 4 + 0.375 for _ in range(num_dimensions)]
-        self.weights: List[float] = [random.random() for _ in range(num_dimensions)]
+        self.weights: List[float] = [
+            (random.random() - 0.5) * 0.01 + 0.5 for _ in range(num_dimensions)
+        ]
 
 
 class SOM:
     def __init__(
         self,
-        num_neurons: int,
+        width: int,
+        height: int,
         num_dimensions: int,
         learning_rate: float,
-        neighbour_radius_decay_rate: float,
         learning_rate_decay_rate: float,
+        neighbour_radius_decay_rate: float,
     ) -> None:
         self.neurons: List[List[Neuron]] = []
-        for i in range(num_neurons):
+        for i in range(height):
             self.neurons.append([])
-            for _ in range(num_neurons):
+            for _ in range(width):
                 self.neurons[i].append(Neuron(num_dimensions))
 
         self.learning_rate = learning_rate
-        self.neighbour_radius: float = num_neurons
+        self.neighbour_radius: float = sqrt(width * height) / 2
         self.neigbour_radius_decay_rate: float = neighbour_radius_decay_rate
         self.learning_rate_decay_rate: float = learning_rate_decay_rate
 
     def neighbour_function(self, distance: float) -> float:
-        sigma: float = self.neighbour_radius / 2
-        return exp(-distance**2 / (2 * sigma**2))
+        sigma: float = self.neighbour_radius
+        return exp(-(distance**2) / (2 * sigma**2))
 
-    def learn(self, input: List[float]) -> None:
+    def find_closest(self, input: List[float]):
         min_idx_i, min_idx_j = 0, 0
         min_dist: float = calculate_distance(
             self.neurons[min_idx_i][min_idx_j].weights, input
@@ -46,48 +49,53 @@ class SOM:
                     min_idx_i, min_idx_j = i, j
                     min_dist = dist
 
-        neighbour_radius: int = int(self.neighbour_radius)
-        for i in range(min_idx_i - neighbour_radius, min_idx_j + neighbour_radius):
-            if i >= 0 and i < len(self.neurons):
-                for j in range(
-                    min_idx_j - neighbour_radius, min_idx_j + neighbour_radius
-                ):
-                    distance: float = sqrt((min_idx_i - i) ** 2 + (min_idx_j - j) ** 2)
-                    if j >= 0 and j < len(self.neurons):
-                        if distance < neighbour_radius:
-                            neuron = self.neurons[i][j]
-                            delta: List[float] = [
-                                (input[i] - neuron.weights[i])
-                                * self.learning_rate
-                                * self.neighbour_function(distance)
-                                for i, _ in enumerate(neuron.weights)
-                            ]
-                            self.neurons[i][j].weights = [
-                                neuron.weights[i] + delta[i] for i, _ in enumerate(neuron.weights)
-                            ]
+        return min_idx_i, min_idx_j
+
+    def learn(self, input: List[float]) -> None:
+        min_idx_i, min_idx_j = self.find_closest(input)
+
+        for row_idx, row in enumerate(self.neurons):
+            for neuron_idx, neuron in enumerate(row):
+                grid_distance: float = sqrt(
+                    (min_idx_i - row_idx) ** 2 + (min_idx_j - neuron_idx) ** 2
+                )
+                if grid_distance < self.neighbour_radius:
+                    neigbourhood: float = self.neighbour_function(grid_distance)
+                    neuron.weights = [
+                        w + (x - w) * self.learning_rate * neigbourhood
+                        for w, x in zip(neuron.weights, input)
+                    ]
 
         self.learning_rate *= self.learning_rate_decay_rate
         self.neighbour_radius *= self.neigbour_radius_decay_rate
 
     def draw(self, canvas: Canvas, neuron_size: int) -> None:
         canvas.delete("all")
+
         for i in range(len(self.neurons)):
             for j in range(len(self.neurons[i])):
                 neuron = self.neurons[i][j]
                 x = neuron.weights[0] * 1000
                 y = neuron.weights[1] * 1000
-                canvas.create_oval(x - neuron_size, y - neuron_size, x + neuron_size, y + neuron_size, fill="blue")
-                if (i+1 < len(self.neurons)):
-                    x1: float = self.neurons[i+1][j].weights[0] * 1000
-                    y1: float = self.neurons[i+1][j].weights[1] * 1000
+                canvas.create_oval(
+                    x - neuron_size,
+                    y - neuron_size,
+                    x + neuron_size,
+                    y + neuron_size,
+                    fill="blue",
+                )
+                if i + 1 < len(self.neurons):
+                    x1: float = self.neurons[i + 1][j].weights[0] * 1000
+                    y1: float = self.neurons[i + 1][j].weights[1] * 1000
                     canvas.create_line(x, y, x1, y1)
-                if (j+1 < len(self.neurons[i])):
-                    x1: float = self.neurons[i][j+1].weights[0] * 1000
-                    y1: float = self.neurons[i][j+1].weights[1] * 1000
+                if j + 1 < len(self.neurons[i]):
+                    x1: float = self.neurons[i][j + 1].weights[0] * 1000
+                    y1: float = self.neurons[i][j + 1].weights[1] * 1000
                     canvas.create_line(x, y, x1, y1)
+
 
 def calculate_distance(a: List[float], b: List[float]) -> float:
     sum: float = 0.0
     for i, j in zip(a, b):
         sum += (i - j) ** 2
-    return sum
+    return sqrt(sum)
